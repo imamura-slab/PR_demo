55259275e39050cccf839966397837833005aae9
imamura-slab <atuuyarumami0704@yahoo.co.jp>
2021-05-07 15:06:16 +0900

    Delete comment in top.sv & Add cntdown_seg7led.sv

---
+++ src/cntdown_seg7.sv
@@ -0,0 +1,44 @@
+`default_nettype none
+module cntdown_seg7
+   (
+    input wire 	      clk,
+    input wire 	      n_rst,
+    input wire 	      one_sec_flag,
+    output wire [3:0] seg7val_out[1:0]
+    );
+
+   reg [3:0] 		     seg7val_reg[1:0];
+
+
+   always_ff@(posedge clk) begin
+      if(!n_rst) begin
+	 seg7val_reg[0] <= '0;
+	 seg7val_reg[1] <= '0;
+      end
+      else begin
+	 if(one_sec_flag) begin
+	    if(seg7val_reg[0] == 'd0) begin
+	       seg7val_reg[0] <= 'd9;
+	       if(seg7val_out[1] == 'd0)
+		 seg7val_reg[1] <= 'd9;
+	       else
+		 seg7val_reg[1] <= seg7val_reg[1] - 'd1;
+	    end
+	    else begin
+	       seg7val_reg[0] <= seg7val_reg[0] - 'd1;
+	       seg7val_reg[1] <= seg7val_reg[1];
+	    end // else: !if(seg7val_reg[0] == 'd0)
+	 end // if (one_sec_flag)
+	 else begin
+	    seg7val_reg[0] <= seg7val_reg[0];
+	    seg7val_reg[1] <= seg7val_reg[1];
+	 end // else: !if(one_sec_flag)
+      end // else: !if(!n_rst)
+   end // always_ff@ (posedge clk)
+
+   
+   assign seg7val_out[0] = seg7val_reg[0];
+   assign seg7val_out[1] = seg7val_reg[1];
+   
+endmodule // cntdown_seg7
+`default_nettype wire
--- src/top.sv
+++ src/top.sv
@@ -11,33 +11,28 @@ module top
    parameter integer 	     FREQUENCY = 50 * 10**6;
 
 
-
+   // count 1 second
    wire 		     one_sec_flag;
    cnt_sec
      #(.FREQUENCY(FREQUENCY))
    cnt_sec_inst
      (.clk(clk), .n_rst(btn[0]), .one_sec_flag_out(one_sec_flag));
 
-   
+
+   // count up 7segLEDs
    wire [3:0] 		     seg7val[1:0];
    cntup_seg7 cntup_seg7_inst
      (.clk(clk), .n_rst(btn[0]), .one_sec_flag(one_sec_flag), .seg7val_out(seg7val));
    
+   // 7seg decorder
+   seg7dec seg7dec_inst
+     (.seg7val_in(seg7val), .seg7led_out(seg7led));
 
+   
+   // count up LEDs
    cntup_led cntup_led_inst
      (.clk(clk), .n_rst(btn[0]), .one_sec_flag(one_sec_flag), .led_out(led));
    
-   
-   // cnt_default
-   //   #(.FREQUENCY(FREQUENCY))
-   // cnt_default_inst
-   //   (.clk(clk), .n_rst(btn[0]), .seg7val_out(seg7val), .led_out(led));
-
-
-   seg7dec seg7dec_inst
-     (.seg7val_in(seg7val), .seg7led_out(seg7led));
-   
-
    
 endmodule // top
 
